bc7e1605200cef36990465723e1a1bfd9be01b16
Ervin Pepic <ervin.hack@gmail.com>
2017-11-21 15:24:35 +0100

    Added new app menus and linking fields from restaurants to new menu items

---
+++ trydjango1-11/src/menus/models.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.conf import settings
+from restaurants.models import RestaurantLocation
+
+class Item(models.Model):
+	user			= models.ForeignKey(settings.AUTH_USER_MODEL)
+	restaurant  	= models.ForeignKey(RestaurantLocation)
+	name			= models.CharField(max_length=120)
+	content			= models.TextField(help_text='Separate each item by comma')
+	exclude			= models.TextField(blank=True, null=True, help_text='Separate each item by comma')
+	public 			= models.BooleanField(default=True)
+	timestamp 		= models.DateTimeField(auto_now_add=True)
+	updated 		= models.DateTimeField(auto_now=True)
+
+
+	class Meta:
+		ordering = ['-updated', '-timestamp']
+	
+
+	def get_content(self):
+		return self.content.split(",")
+
+	def get_exclude(self):
+		return self.exclude.split(",")
