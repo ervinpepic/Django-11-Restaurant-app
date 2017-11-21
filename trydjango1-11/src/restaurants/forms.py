03e2e4029c3013361bc9e7cf6527589cba860abb
Ervin Pepic <ervin.hack@gmail.com>
2017-11-21 12:48:54 +0100

    Added validators.py for forms and using validation in models.py

--- trydjango1-11/src/restaurants/forms.py
+++ trydjango1-11/src/restaurants/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import RestaurantLocation
+from .validators import validate_category
 
 # class RestaurantCreateForm(forms.Form):
 # 	name 		= forms.CharField()
@@ -14,6 +15,8 @@ from .models import RestaurantLocation
 
 
 class RestaurantLocationCreateForm(forms.ModelForm):
+	# email 		= forms.EmailField()
+	#category 		= forms.CharField(required=False, validators=[validate_category])
 	class Meta:
 		model = RestaurantLocation
 		fields = [
@@ -21,9 +24,20 @@ class RestaurantLocationCreateForm(forms.ModelForm):
 			'location',
 			'category'
 		]
-	
+
 	def clean_name(self):
 		name = self.cleaned_data.get("name")
 		if name == "hello":
 			raise forms.ValidationError("not a valid name")
 		return name
+
+
+	# def clean_email(self):
+	# 	email = self.cleaned_data.get("email")
+	# 	if ".edu" in email:
+	# 		raise forms.ValidationError(
+	# 			"We don't accept education email domain. Please use google instead!"
+	# 			)
+	# 	return email
+
+
